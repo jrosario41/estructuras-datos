be42252e6458e0b56d61b79075ca2ebec3feaf5d
jrosario41 <58098523+jrosario41@users.noreply.github.com>
2025-09-13 15:52:00 -0400

    Add files via upload

---
+++ arreglos-listas/app.js
@@ -0,0 +1,24 @@
+function mensaje(texto){
+    console.log(texto);
+    alert(texto);
+    const resultado = document.getElementById("txtresultado");
+    resultado.textContent = texto;
+
+}
+
+function Btnagregar (palabra){
+    
+}
+
+function Btneliminar (palabra){
+    
+}
+
+function Btnmostrar (palabra){
+    
+}
+
+function Btnmodificar (palabra){
+    
+}
+
